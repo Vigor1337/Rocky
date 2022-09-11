4e829c106da782a7d1b6a51f162e33124882deb5
Vigor1337 <113300426+Vigor1337@users.noreply.github.com>
2022-09-11 17:13:54 +0100

    Create notify.lua

---
+++ src/UI/notify.lua
@@ -0,0 +1,29 @@
+local Rocky = script:FindFirstAncestor("Rocky")
+
+local Notifications = {}
+
+function Notifications:Notify(M,R,G,B)
+    spawn(function()
+		repeat wait() until not NOTIFY_1
+		local NOTIFY_SETH = GUIS.notify_seth:Clone() NOTIFY_SETH.Parent = game.CoreGui
+		if NOTIFY_SETH then
+			NOTIFY_SETH.notify[''].BackgroundColor3 = C3(R, G, B)
+			NOTIFY_SETH.notify.text.Text = ' ' .. M
+			repeat wait() until not NOTIFY_1
+			NOTIFY_1 = true
+			wait(0.5)
+			NOTIFY_SETH.notify:TweenPosition(UDim2.new(0, 0, 0.6, 0), 'InOut', 'Quad', 0.4, false) wait(0.5)
+			wait(0.5)
+			repeat wait() until not NOTIFY_2
+			NOTIFY_1 = false
+			NOTIFY_SETH.notify:TweenPosition(UDim2.new(0, 0, 0.6, -40), 'InOut', 'Quad', 0.4, false) wait(0.5)
+			wait(0.5)
+			NOTIFY_2 = true
+			wait(2.5)
+			NOTIFY_SETH.notify:TweenPosition(UDim2.new(0, -225, 0.6, -40), 'InOut', 'Quad', 0.4, false) wait(0.5)
+		end
+		wait(1)
+		NOTIFY_SETH:destroy()
+		NOTIFY_2 = false
+	end)
+end
