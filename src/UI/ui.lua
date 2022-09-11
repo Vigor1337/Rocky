143fa42274171e1c16ed2b9692ab2ba8435053b3
Vigor1337 <113300426+Vigor1337@users.noreply.github.com>
2022-09-11 18:22:04 +0100

    Update ui.lua

--- src/UI/ui.lua
+++ src/UI/ui.lua
@@ -4,7 +4,9 @@ local UI = {}
 
 function UI:Init()
     local UI = Rocky.Rocky:Clone()
+    UI["seth_main"].Enabled = false
     UI.Parent = game.CoreGui
+    UI["cmdbar_seth"].bar:TweenPosition(UDim2.new(0, 0, 1, -50), 'InOut', 'Quad', 0.5, true)
 end
 
 return UI
