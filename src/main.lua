d32d0c4181373f8a2438b337f713ae5ac4fd2406
Vigor1337 <113300426+Vigor1337@users.noreply.github.com>
2022-09-11 21:04:48 +0100

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,12 +1,14 @@
 local Rocky = script:FindFirstAncestor("Rocky")
 local ST = os.clock()
 local UI = require(Rocky.UI.ui)
+local CMDBar = require(Rocky.UI.cmdbar)
 local Noti = require(Rocky.UI.notify)
 
 local Main = {}
 
 function Main:Init()
     UI:Init()
+    CMDBar:Init()
     Noti:Notify('Hello, ' .. game:GetService("Players").LocalPlayer.Name, 255, 255, 255)
     Noti:Notify('Script loaded!', 50, 255, 50)
 end
